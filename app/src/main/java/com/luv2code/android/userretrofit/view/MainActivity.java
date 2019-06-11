4f72ea903c7510cde65a611a308d30b5759c0f67
Luka Žugaj <lzugaj@tvz.hr>
2019-06-11 21:46:31 +0200

    Delete unecessary import

--- app/src/main/java/com/luv2code/android/userretrofit/view/MainActivity.java
+++ app/src/main/java/com/luv2code/android/userretrofit/view/MainActivity.java
@@ -24,7 +24,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
